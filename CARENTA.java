72fb869e19f20be59e3c61793251fbc2366aa061
Yolof <slask@yolof.se>
2013-12-10 00:14:20 +0100

    Main method of CARENTA car rental system.
    Just a draft...

---
+++ CARENTA.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CARENTA extends JFrame {
+
+	JPanel buttonPanel = new JPanel();
+	JButton customerButton = new JButton("Kund");
+
+	public CARENTA() {
+
+    buttonPanel.add(customerButton);
+    add(buttonPanel);
+	customerButton.setLocation(0,0);
+    
+    
+    setSize(400, 400);
+    setBackground(Color.BLACK);
+    setTitle("Air Traffic Control");
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+	}
+
+	public static void main(String[] args) {
+
+    new CARENTA();
+
+	}
+}
