f0240becdf85bc4ed51013720f04023f11852f26
Yolof <slask@yolof.se>
2013-12-10 20:15:24 +0100

    Update Vehicle.java

--- Source-files/Vehicle.java
+++ Source-files/Vehicle.java
@@ -1,87 +1,101 @@
+/* This class describes a vehicle with the following attributes and methods! */
+
 public class Vehicle {
-	
+
 	private String regNbr;
-	private String model;
-	private String type;
-	private String licenseReq;
-	private int price;
-	private String info;
-	private boolean hasHook;
-	private String expiryDate;
-        
+	private String model; 
+	private String type; // What kind of vehicle...
+	private String licenseReq; // What license is required to drive it...
+	private int price; // Rental price per day...
+	private String info; // General information about the vehicle
+	private boolean hasHook; // If the vehicle is able to pull lorries...
+	private String expiryDate; // When it should be sold!
+	private Warehouse warehouse; // Where the vehicle is located!
+
 	public Vehicle(String regNbr, String model, String type, String licenseReq,
-                   int price, String info, boolean hasHook, String expiryDate){
-    	this.regNbr = regNbr;
-    	this.model = model;
-    	this.type = type;
-        this.licenseReq = licenseReq;
-        this.price = price;
-        this.info = info;
-        this.hasHook = hasHook;
-        this.expiryDate = expiryDate;
+				   int price, String info, boolean hasHook, 
+				   String expiryDate, Warehouse warehouse) {
+		
+		this.regNbr = regNbr;
+		this.model = model;
+		this.type = type;
+		this.licenseReq = licenseReq;
+		this.price = price;
+		this.info = info;
+		this.hasHook = hasHook;
+		this.expiryDate = expiryDate;
+		this.warehouse = warehouse;
 	}
-        
+
 	public String getRegNbr() {
 		return regNbr;
 	}
-        
+
 	public void setRegNbr(String regNbr) {
 		this.regNbr = regNbr;
 	}
-        
+
 	public String getModel() {
-	    return model;
+		return model;
 	}
-        
+
 	public void setModel(String model) {
 		this.model = model;
 	}
-        
+
 	public String getType() {
 		return type;
 	}
-        
+
 	public void setType(String type) {
 		this.type = type;
 	}
-        
+
 	public String getLicenseReq() {
 		return licenseReq;
 	}
-        
+
 	public void setLicenseReq(String licenseReq) {
 		this.licenseReq = licenseReq;
 	}
-        
+
 	public int getPrice() {
-        return price;
+		return price;
 	}
-        
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
-        
+
 	public String getInfo() {
 		return info;
 	}
-        
+
 	public void setInfo(String info) {
 		this.info = info;
 	}
-        
+
 	public boolean isHasHook() {
 		return hasHook;
 	}
-        
+
 	public void setHasHook(boolean hasHook) {
 		this.hasHook = hasHook;
 	}
-        
+
 	public String getExpiryDate(){
 		return expiryDate;
 	}
-        
+
 	public void setExpiryDate(String expiryDate){
 		this.expiryDate = expiryDate;
+	}
+	
+	public Warehouse getWarehouse(){
+		return warehouse;
+	}
+
+	public void setWarehouse(Warehouse warehouse){
+		this.warehouse = warehouse;
 	}
 }
