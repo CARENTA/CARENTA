77b39e126d4cd6d5f589877743aece312f5d0896
Yolof <slask@yolof.se>
2013-12-10 20:09:27 +0100

    Update AccessoryRegistry.java

--- Source-files/AccessoryRegistry.java
+++ Source-files/AccessoryRegistry.java
@@ -1,26 +1,32 @@
+/* This class describes a registry able to contain accessories within an array list! */ 
+
 import java.util.ArrayList;
 
 public class AccessoryRegistry {
-	
+
 	private ArrayList<Accessory> accessories;
 
-	public AccessoryRegistry(ArrayList<Accessory> accessories) {
-		this.accessories = accessories;
+	public AccessoryRegistry() {
+		this.accessories = new ArrayList<Accessory>(); // Creates an empty array list...
 	}
 
-	public ArrayList<Accessory> getvehicles() {
+	public ArrayList<Accessory> getAccessories() { // Returns the list containing ALL accessories!
 		return accessories;
 	}
 
-	public void setvehicles(ArrayList<Accessory> accessories) {
+	public void setAccessories(ArrayList<Accessory> accessories) { // Sets a list containing accessories!
 		this.accessories = accessories;
 	}
-	
-	public void addvehicle(Accessory vehicle) {
-		accessories.add(vehicle);
+
+	public void addAccessory(Accessory accessory) { // Adds a single accessory to the registry!
+		accessories.add(accessory);
 	}
 	
-	public void removevehicle(Accessory vehicle) {
-		accessories.remove(vehicle);
+	public Accessory getAccessory(int pos) { // Returns a single accessory from the position "pos" within the registry!
+		return accessories.get(pos);
+	}
+
+	public void removeAccessory(Accessory accessory) { // Removes the accessory from the list!
+		accessories.remove(accessory);
 	}
 }
