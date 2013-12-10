ac967e7080eaa03991b8e04822f7fae019c48b30
jonasmellstrom <jonas.mellstrom@hotmail.com>
2013-12-10 15:48:45 +0100

    Update Employee.java

--- Source-files/Employee.java
+++ Source-files/Employee.java
@@ -1,83 +1,80 @@
+public abstract class Employee {
+  
+        private String employeeNbr;
+        private String personalNbr;
+        private String firstName;
+        private String lastName;
+        private String adress;
+        private String email;
+        private String telephoneNbr;
+       
+        
+        public Employee(String employeeNbr, String personalNbr, String firstName, String lastName, String adress,
+                        String email, String telephoneNbr){
+                this.setEmployeeNbr(employeeNbr);
+                this.setPersonalNbr(personalNbr);
+                this.setFirstName(firstName);
+                this.setLastName(lastName);
+                this.setAdress(adress);
+                this.setEmail(email);
+                this.setTelephoneNbr(telephoneNbr);
+        }
 
-public class Employee {
-	// Klassen Employee består av anställda på kontoret för att kunna logga vad olika anställda har för info
-	// och kunna registrera vad de anställda har gjort för affärer.
-	private String employeeNbr;
-	private String personalNbr;
-	private String firstName;
-	private String lastName;
-	private String adress;
-	private String email;
-	private String telephoneNbr;
-	// Ange vilka variablar vi använder. Se UML diagram för klassen och variablar.
-	
-	public Employee(String employeeNbr, String personalNbr, String firstName, String lastName, String adress,
-			String email, String telephoneNbr){
-		this.setEmployeeNbr(employeeNbr);
-		this.setPersonalNbr(personalNbr);
-		this.setFirstName(firstName);
-		this.setLastName(lastName);
-		this.setAdress(adress);
-		this.setEmail(email);
-		this.setTelephoneNbr(telephoneNbr);
-	}
+        public String getEmployeeNbr() {
+                return employeeNbr;
+        }
 
-	public String getEmployeeNbr() {
-		return employeeNbr;
-	}
+        public void setEmployeeNbr(String employeeNbr) {
+                this.employeeNbr = employeeNbr;
+        }
 
-	public void setEmployeeNbr(String employeeNbr) {
-		this.employeeNbr = employeeNbr;
-	}
+        public String getPersonalNbr() {
+                return personalNbr;
+        }
 
-	public String getPersonalNbr() {
-		return personalNbr;
-	}
+        public void setPersonalNbr(String personalNbr) {
+                this.personalNbr = personalNbr;
+        }
 
-	public void setPersonalNbr(String personalNbr) {
-		this.personalNbr = personalNbr;
-	}
+        public String getFirstName() {
+                return firstName;
+        }
 
-	public String getFirstName() {
-		return firstName;
-	}
+        public void setFirstName(String firstName) {
+                this.firstName = firstName;
+        }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+        public String getLastName() {
+                return lastName;
+        }
 
-	public String getLastName() {
-		return lastName;
-	}
+        public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+        public String getAdress() {
+                return adress;
+        }
 
-	public String getAdress() {
-		return adress;
-	}
+        public void setAdress(String adress) {
+                this.adress = adress;
+        }
 
-	public void setAdress(String adress) {
-		this.adress = adress;
-	}
+        public String getEmail() {
+                return email;
+        }
 
-	public String getEmail() {
-		return email;
-	}
+        public void setEmail(String email) {
+                this.email = email;
+        }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+        public String getTelephoneNbr() {
+                return telephoneNbr;
+        }
 
-	public String getTelephoneNbr() {
-		return telephoneNbr;
-	}
-
-	public void setTelephoneNbr(String telephoneNbr) {
-		this.telephoneNbr = telephoneNbr;
-	}
-	// Skapar setter och getter för Employee klassen.
-	
+        public void setTelephoneNbr(String telephoneNbr) {
+                this.telephoneNbr = telephoneNbr;
+        }
 
 }
+        
