875fe8fab3942883b1bbd5635a7011215ab84369
Yolof <slask@yolof.se>
2013-12-10 20:11:24 +0100

    Update CustomerRegistry.java

--- Source-files/CustomerRegistry.java
+++ Source-files/CustomerRegistry.java
@@ -1,26 +1,32 @@
+/* This class describes a registry able to contain customers within an array list! */ 
+
 import java.util.ArrayList;
 
 public class CustomerRegistry {
-	
+
 	private ArrayList<Customer> customers;
 
-	public CustomerRegistry(ArrayList<Customer> customers) {
-		this.customers = customers;
+	public CustomerRegistry() {
+		this.customers = new ArrayList<Customer>(); // Creates an empty array list..
 	}
 
-	public ArrayList<Customer> getCustomers() {
+	public ArrayList<Customer> getCustomers() { // Returns the list containing ALL customers!
 		return customers;
 	}
 
-	public void setCustomers(ArrayList<Customer> customers) {
+	public void setCustomers(ArrayList<Customer> customers) { // Sets a list containing customers!
 		this.customers = customers;
 	}
-	
-	public void addCustomer(Customer customer) {
+
+	public void addCustomer(Customer customer) { // Adds a single customer to the registry!
 		customers.add(customer);
 	}
+
+	public Customer getCustomer(int pos) { // Returns a single customer from the position "pos" within the registry!
+		return customers.get(pos);
+	}
 	
-	public void removeCustomer(Customer customer) {
+	public void removeCustomer(Customer customer) { // Removes the customer from the list!
 		customers.remove(customer);
 	}
 }
