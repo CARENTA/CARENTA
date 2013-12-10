2b642890b8ca771d11e639903ab57e0b4e0fbe70
Yolof <slask@yolof.se>
2013-12-10 20:15:49 +0100

    Update VehicleRegistry.java

--- Source-files/VehicleRegistry.java
+++ Source-files/VehicleRegistry.java
@@ -1,26 +1,33 @@
+/* This class describes a registry able to contain vehicles within an array list! */ 
+
 import java.util.ArrayList;
 
 public class VehicleRegistry {
-	
+
 	private ArrayList<Vehicle> vehicles;
-
-	public VehicleRegistry(ArrayList<Vehicle> vehicles) {
-		this.vehicles = vehicles;
+	
+	public VehicleRegistry() {
+		this.vehicles = new ArrayList<Vehicle>(); // Creates an empty array list...
 	}
-
-	public ArrayList<Vehicle> getvehicles() {
+	
+	public ArrayList<Vehicle> getVehicles() { // Returns the list containing ALL vehicles!
 		return vehicles;
 	}
 
-	public void setvehicles(ArrayList<Vehicle> vehicles) {
+	public void setVehicles(ArrayList<Vehicle> vehicles) { // Sets a list containing vehicles!
 		this.vehicles = vehicles;
 	}
-	
-	public void addvehicle(Vehicle vehicle) {
+
+	public void addVehicle(Vehicle vehicle) { // Adds a single vehicle to the registry!
 		vehicles.add(vehicle);
 	}
+
+	public Vehicle getVehicle(int pos) { // Returns a single vehicle from the position "pos" within the registry!
+		return vehicles.get(pos);
+	}
 	
-	public void removevehicle(Vehicle vehicle) {
+	public void removeVehicle(Vehicle vehicle) { // Removes the vehicle from the list!
 		vehicles.remove(vehicle);
 	}
+	
 }
