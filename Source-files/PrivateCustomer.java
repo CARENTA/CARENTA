9ba1d64a36b92e74d236108f960f30f9aeec3683
Yolof <slask@yolof.se>
2013-12-24 20:21:54 +0100

    Update PrivateCustomer.java

--- Source-files/PrivateCustomer.java
+++ Source-files/PrivateCustomer.java
@@ -1,25 +1,47 @@
 /* This class describes a private customer with the following attributes and methods! */
 
+import java.util.ArrayList;
+
 public class PrivateCustomer extends Customer {
 
 	private String personNbr; // Identification variable! 
+	private String firstName;
+	private String lastName;
 	
-	public PrivateCustomer(String firstName, String lastName,
-			String customerNbr, String adress, String city, String areaCode,
-			String phoneNbr, String mailAdress, int discountLevel,
-			Order previousOrder, String personNbr) {
+	public PrivateCustomer(int customerNbr, String personNbr, String firstName, 
+			String lastName, String adress, String city, String areaCode,
+			String phoneNbr, String mailAdress, int discountLevel) {
 		
-		super(firstName, lastName, customerNbr, adress, city, areaCode, phoneNbr, // Heritage...
+		super(customerNbr, adress, city, areaCode, phoneNbr,
 				mailAdress, discountLevel);
 		
-		this.personNbr = personNbr;	
+		this.personNbr = personNbr;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		
 	}
 
-	public String getOrgNbr() {
+	public String getPersonNbr() {
 		return personNbr;
 	}
 
-	public void setOrgNbr(String personNbr) {
+	public void setPersonNbr(String personNbr) {
 		this.personNbr = personNbr;
-	}	
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
