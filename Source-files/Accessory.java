364a8c500bf60b0765f5a1002120887bbcb47ecd
Yolof <slask@yolof.se>
2013-12-24 16:20:39 +0100

    Update Accessory.java

--- Source-files/Accessory.java
+++ Source-files/Accessory.java
@@ -2,41 +2,45 @@
 /* This class describes an accessory with the following attributes and methods! */
 
 public class Accessory extends Product {
-        
-        private int productNbr;
-        private int price;
-        private String name;
-        private String info;
 
-        public Accessory(int productNbr, int price, String name, String info) {
-                this.productNbr = productNbr;
-                this.price = price;
-                this.name = name;
-                this.info = info;
-        }
+	private int productNbr;
+	private String name;
+	private int price;
+	private String info;
 
-        public int getProductNbr() {
-                return productNbr;
-        }
-        public void setProductNbr(int productNbr) {
-                this.productNbr = productNbr;
-        }
-        public int getPrice() {
-                return price;
-        }
-        public void setPrice(int price) {
-                this.price = price;
-        }
-        public String getName() {
-                return name;
-        }
-        public void setName(String name) {
-                this.name = name;
-        }
-        public String getInfo() {
-                return info;
-        }
-        public void setInfo(String info) {
-                this.info = info;
-        }
+	public Accessory(int productNbr, String name, int price, String info) {
+		this.productNbr = productNbr;
+		this.name = name;
+		this.price = price;
+		this.info = info;
+	}
+
+	public int getProductNbr() {
+		return productNbr;
+	}
+	public void setProductNbr(int productNbr) {
+		this.productNbr = productNbr;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
 }
