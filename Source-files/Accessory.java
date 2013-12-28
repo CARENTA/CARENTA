fe398c9759cbe7f517cb40618b2cfe13f9a708db
Yolof <slask@yolof.se>
2013-12-28 02:32:22 +0100

    Update Accessory.java

--- Source-files/Accessory.java
+++ Source-files/Accessory.java
@@ -1,46 +1,21 @@
-
 /* This class describes an accessory with the following attributes and methods! */
 
 public class Accessory extends Product {
 
-	private int productNbr;
-	private String name;
-	private int price;
-	private String info;
+	private int productNbr; // Used for identifying which product...
 
-	public Accessory(int productNbr, String name, int price, String info) {
+	public Accessory(int productNbr, String productName, int price, String infoTxt) {
+		
+		super(productName, infoTxt, price); // Heritage...
+		
 		this.productNbr = productNbr;
-		this.name = name;
-		this.price = price;
-		this.info = info;
 	}
 
 	public int getProductNbr() {
 		return productNbr;
 	}
+
 	public void setProductNbr(int productNbr) {
 		this.productNbr = productNbr;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-	public void setInfo(String info) {
-		this.info = info;
 	}
 }
