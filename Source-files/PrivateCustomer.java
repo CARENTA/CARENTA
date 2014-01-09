77e7c0328c737fcfa4d17a761e82a69ff79ceaba
jonasmellstrom <jonas.mellstrom@hotmail.com>
2014-01-09 22:00:04 +0100

    Update PrivateCustomer.java

--- Source-files/PrivateCustomer.java
+++ Source-files/PrivateCustomer.java
@@ -1,45 +1,37 @@
 /* This class describes a private customer with the following attributes and methods! */
 
 public class PrivateCustomer extends Customer {
-
-	private String personNbr; // Identification variable! 
-	private String firstName;
-	private String lastName;
 	
-	public PrivateCustomer(int customerNbr, String personNbr, String firstName, 
-			String lastName, String adress, String city, String areaCode,
-			String phoneNbr, String mailAdress, int discountLevel) {
-		
-		super(customerNbr, adress, city, areaCode, phoneNbr,
-				mailAdress, discountLevel);
-		
-		this.personNbr = personNbr;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		
-	}
+        private String firstName;
+        private String lastName;
+        
+        public PrivateCustomer(int customerNbr, String IDNbr, String firstName, 
+                        String lastName, String adress, String city, String areaCode,
+                        String phoneNbr, String mailAdress, int discountLevel) {
+                
+                super(customerNbr, IDNbr, adress, city, areaCode, phoneNbr,
+                                mailAdress, discountLevel);
+                
+           
+                this.firstName = firstName;
+                this.lastName = lastName;
+                
+        }
 
-	public String getPersonNbr() {
-		return personNbr;
-	}
 
-	public void setPersonNbr(String personNbr) {
-		this.personNbr = personNbr;
-	}
+        public String getFirstName() {
+                return firstName;
+        }
 
-	public String getFirstName() {
-		return firstName;
-	}
+        public void setFirstName(String firstName) {
+                this.firstName = firstName;
+        }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+        public String getLastName() {
+                return lastName;
+        }
 
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+        public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
 }
